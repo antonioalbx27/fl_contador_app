27d1f87bf5175b13288ba830ce1339644064b91e
antonioalbx27 <antonioalbarran2704@gmail.com>
2025-10-21 10:09:11 +0200

    Cambios realizados con los botones

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,23 +1,45 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int contador = 0;
+  @override
   Widget build(BuildContext context) {
+    const tamano = TextStyle(fontSize: 30);
+
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      appBar: AppBar(title: Text('HomeScreen'), elevation: 10.0),
+      appBar: AppBar(title: Text('Contador'), elevation: 10.0),
 
       body: Center(
-        child: const Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('I cant wait to watch you die'),
-            SizedBox(height: 100),
-            Text('I cant wait to watch you die2'),
+            Text('numero de toques', style: tamano),
+            SizedBox(height: 50),
+            Text('$contador', style: tamano),
           ],
         ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Row(
+        children: [
+          FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () {
+              print('Se detecta una pulsacion');
+              contador++;
+              setState(() {});
+              print('$contador');
+            },
+          ),
+        ],
       ),
     );
   }
